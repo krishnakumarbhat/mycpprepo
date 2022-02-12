d2d064550b87c4c4924b99fb8b299a190607dfb0
krishna <79183768+krishnakumarbhat@users.noreply.github.com>
2022-02-12 16:40:24 +0530

    Update sum of digit in number.cpp
    added reverse the number

--- sum of digit in number.cpp
+++ sum of digit in number.cpp
@@ -17,3 +17,17 @@ int main(){
 		}
 	}
 
+
+	int a,y=0,v,sum=0;
+	cin>>a;
+	for(int i=a;i>0;i--){
+			cin>>v;
+			sum=0;
+			while(v>0){
+			y=v%10;
+			v=v/10;
+			sum = (sum*10)+y;
+			
+		}
+		cout<<sum<<endl;
+	}
