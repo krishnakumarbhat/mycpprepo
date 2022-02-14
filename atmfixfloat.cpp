6fdd1a1666c7adb3b9f3cd15c891e7bba1500eea
krishnakumar bhat <79183768+krishnakumarbhat@users.noreply.github.com>
2022-02-14 16:14:14 +0530

    Create atmfixfloat.cpp

---
+++ atmfixfloat.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int a;
+	double b,c;
+	cin>>a>>b;
+	c =b-a-0.5;
+	if (a%5== 0 & (b>a)){
+
+	if (b>a){
+		cout<<c<<endl;
+	}
+	else{
+		cout<<b
+		<< std::fixed<<endl;
+	}
+
+	}
+	else{
+		cout<< std::fixed
+		<< b<<endl;
+	}
+}
+
+
+            
+            // << std::setprecision((modf(b, &mod) != 0.0) ? 2 : 0)
+            // << avg
+            // << std::endl;
